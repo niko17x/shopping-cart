89135b0b617a947636a5cd3a65ddb0ee3f032f1a
Niko Shin <nshin17@gmail.com>
2023-04-25 21:33:48 -0700

    Linked button to ShoppinPage.js component.

--- src/components/ShopNowButton.js
+++ src/components/ShopNowButton.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ShopNowButton() {
   const shopNowButton = () => (
-    <button className="button--shop_now">Shop Now</button>
+    <Link to={"/shop"}>
+      <button className="button--shop_now">Shop Now</button>
+    </Link>
   );
 
   return <div className="shop_now_button--container">{shopNowButton()}</div>;
